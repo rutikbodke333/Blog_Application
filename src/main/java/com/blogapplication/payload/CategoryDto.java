0e8ceb4e7378c786af80c9249dbd29d8fe7ce9fb
rutikbodke333 <rutikbodke333@gmail.com>
2025-06-14 13:34:28 +0530

    add validation to UserDto, PostDto, CommentDto, CategoryDto and update .classpath

--- src/main/java/com/blogapplication/payload/CategoryDto.java
+++ src/main/java/com/blogapplication/payload/CategoryDto.java
@@ -14,11 +14,11 @@ public class CategoryDto {
 	private Integer categoryId;
 
 	@NotBlank
-	@Size(min = 4, message = "Title must be min of 4 characters !!")
+	@Size(min = 4, max = 20, message = "category title must be min of 4 characters and max of 20 characters !!")
 	private String categoryTitle;
 
 	@NotBlank
-	@Size(min = 10, message = "Description must be min of 10 characters !!")
+	@Size(min = 10, max = 500, message = "category description must be min of 10 characters and max of 500 characters !!")
 	private String categoryDescription;
 
 }
